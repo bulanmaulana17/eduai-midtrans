e469667a0e5f5e34341e4fd1deb62772c9d48ee1
bulanmaulana17 <96629939+bulanmaulana17@users.noreply.github.com>
2025-08-08 20:25:03 +0700

    Update create-transaction.js

--- eduai-midtrans/api/create-transaction.js
+++ eduai-midtrans/api/create-transaction.js
@@ -3,7 +3,7 @@ import midtransClient from 'midtrans-client';
 
 export default async function handler(req, res) {
   // 1. TAMBAHKAN HEADER CORS DI SINI (SEBELUM PROSES UTAMA)
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://eduai-generator.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   
